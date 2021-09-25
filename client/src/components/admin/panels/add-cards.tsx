747ab72afa52d843938db5292895d997ff945fdb
Brendon Lapp <brendon.lapp@gmail.com>
2021-09-24 21:38:47 -0600

    Added tabbing for the admin pages

--- client/src/components/admin/panels/add-cards.tsx
+++ client/src/components/admin/panels/add-cards.tsx
@@ -1,6 +1,16 @@
+import { useState } from 'react';
+import Tabs from '../tab-nav/tabs';
+
 const AddCards = () => {
+  const [tabs] = useState([
+    'Add By CSV',
+    'Add By Card Name',
+    'Update Card Inventory',
+  ]);
+
   return (
     <>
+      <Tabs tabs={tabs} defaultTab={tabs[0]} />
       <div>Add Cards</div>
     </>
   );
--- client/src/components/admin/panels/add-sealed.tsx
+++ client/src/components/admin/panels/add-sealed.tsx
@@ -1,6 +1,15 @@
+import { useState } from 'react';
+import Tabs from '../tab-nav/tabs';
+
 const AddSealed = () => {
+  const [tabs] = useState([
+    'Add Sealed Product',
+    'Update Sealed Product Inventory',
+  ]);
+
   return (
     <>
+      <Tabs tabs={tabs} defaultTab={tabs[0]} />
       <div>Sealed Product</div>
     </>
   );
